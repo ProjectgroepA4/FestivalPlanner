b9eb9965dc1f8f527b387ae0c997548eabb17c15
Remco <remcosannen@gmail.com>
2015-03-17 10:30:10 +0100

    Changed to new Podium (Stage)

--- Objects/Podium.java
+++ Objects/Podium.java
@@ -7,7 +7,7 @@ import Applicatie.DrawObject;
 public class Podium extends DrawObject {
 
 	public Podium(Point2D position) {
-		super("images/stage3.png", position);
+		super("images/stage4.png", position);
 	}
 
 }
